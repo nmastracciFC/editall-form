c093e95fc1399ab9f744cf2a873ba557e02c3dff
nmastracci <mastracci.natalie@gmail.com>
2018-04-06 16:37:53 -0400

    found the bug

--- admin/form_val.php
+++ admin/form_val.php
@@ -1,6 +1,8 @@
 <?php
 	require_once('phpscripts/config.php');
 	$errors = array();
+	ini_set('display_errors',1);
+	error_reporting(E_ALL);
 	//echo $ip;
 	if(isset($_POST['submit'])){
 		//echo "Works";
@@ -9,13 +11,18 @@
 		$address = trim($_POST['address']);
 
 		$required = array("name", "phone", "address");
+		// echo $required; die;
 		foreach($required as $require) {
 			$value = trim($_POST[$require]);
 			if(!has_value($value)) {
-				$errors[$require] = $require." cannot be blank.";//if it does not have a value push it into the errors array
+				$errors[$require] = ucfirst($require)." cannot be blank.";//if it does not have a value push it into the errors array
+				// echo $require; die;
+				//ucfirst forces captializes the first letter
 			}
 		}
-		
+		 $max_lengths = array("name" => 15, "phone" => 8);
+		 // var_dump($max_lengths); die;
+		 max_length($max_lengths);
 	}
 ?>
 <!doctype html>
--- admin/phpscripts/val.php
+++ admin/phpscripts/val.php
@@ -19,6 +19,23 @@ function has_value($value) {
 	//this guards against if someone has a space in the form
 }
 
+function max_length($max_lengths) {
+	global $errors;
+	// echo $errors;die;
+	foreach($max_lengths as $value => $max) {
+		$value = trim($_POST[$value]);
+		// echo $value; die;
+		if(!has_proper_length($value, $max)){
+			$errors[$value] = ucfirst($value)." is too long.";
+			//report back to errors array if its too long
+		}
+	}
+}
+
+function has_proper_length($value, $max) {
+	return strlen($value) <= $max;
+}
+
 
 
 
